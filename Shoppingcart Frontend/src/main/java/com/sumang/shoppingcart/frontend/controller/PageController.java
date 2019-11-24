d4c2fb22990d912ec8d6dfa5122e157bda51b4fd
Suman Ghimire <sumanghimire@Sumans-MacBook-Pro.local>
2019-11-23 18:55:24 -0600

    adding a change from the feature branch

---
+++ Shoppingcart Frontend/src/main/java/com/sumang/shoppingcart/frontend/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.sumang.shoppingcart.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+    @RequestMapping(value = {"/", "/home", "/index"})
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("greeting", "Welcome to Xuman Shopping Store.");
+        return mv;
+    }
+
+}
