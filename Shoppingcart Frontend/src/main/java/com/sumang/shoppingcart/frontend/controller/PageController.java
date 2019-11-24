1a6cc32e76a7e97406f55e4ad0457ea9cace7bbd
Suman Ghimire <sumanghimire@Sumans-MacBook-Pro.local>
2019-11-23 22:38:25 -0600

    Added Bootstrap with HTML, CSS and Js

--- Shoppingcart Frontend/src/main/java/com/sumang/shoppingcart/frontend/controller/PageController.java
+++ Shoppingcart Frontend/src/main/java/com/sumang/shoppingcart/frontend/controller/PageController.java
@@ -1,8 +1,12 @@
 package com.sumang.shoppingcart.frontend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.xml.ws.RequestWrapper;
 
 @Controller
 public class PageController {
@@ -13,5 +17,25 @@ public class PageController {
         mv.addObject("greeting", "Welcome to Xuman Shopping Store.");
         return mv;
     }
+
+//    @RequestMapping(value ="/test")
+//    public ModelAndView test (@RequestParam(value="greeting", required=false)String greeting){
+//        if(greeting == null){
+//            greeting = "Welcome to Xumang Store";
+//        }
+//        ModelAndView mv = new ModelAndView("page");
+//        mv.addObject("greeting", greeting);
+//        return mv;
+//    }
+//
+//    @RequestMapping(value ="/test/{greeting}")
+//    public ModelAndView test (@PathVariable("greeting")String greeting){
+//        if(greeting == null){
+//            greeting = "Welcome to Xumang Store";
+//        }
+//        ModelAndView mv = new ModelAndView("page");
+//        mv.addObject("greeting", greeting);
+//        return mv;
+//    }
 
 }
---
+++ Shoppingcart Frontend/web/WEB-INF/views/page1.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sumanghimire
+  Date: 11/23/19
+  Time: 8:05 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:set var="contextRoot" value="${pageContext.request.contextPath}"/>
+
+
+<html>
+<head>
+    <title>Online Shopping</title>
+</head>
+<body>
+${contextRoot} says -  ${greeting}
+</body>
+</html>
